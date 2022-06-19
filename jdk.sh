f59f59b6259ad5820c11e7e9f8be7ddba10cb404
Benjamin Sautner <1295207+bsautner@users.noreply.github.com>
2022-06-19 07:31:01 -0400

    Create jdk.sh

---
+++ jdk.sh
@@ -0,0 +1,11 @@
+wget https://s3.amazonaws.com/www.sautner.me/jdk-11.0.14_linux-aarch64_bin.tar.gz
+sudo mkdir /usr/lib/jvm
+cd /usr/lib/jvm
+sudo tar -xvzf ~/jdk-11.0.14_linux-aarch64_bin.tar.gz
+cd ~
+bash -c 'echo "PATH=/usr/lib/jvm/jdk-11.0.14/bin:$PATH" >> ~/.profile'
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk-11.0.14/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk-11.0.14/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk-11.0.14/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk-11.0.14/bin/javac
+
